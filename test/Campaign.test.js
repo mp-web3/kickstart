55721a1167e8153e3f40c649838d95d11c84544c
mp-web3 <mp.web3@gmail.com>
2023-06-10 17:24:35 +0200

    preparing tests

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -2,3 +2,8 @@ const assert = require('assert');
 const ganache = require('ganache');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider()); /* Create an instance of Web3 */
+
+const compiledFactory = require('../ethereum/build/Factory.json');
+const compiledCampaign = require('../ethereum/build/Campaign.json');
+
+
