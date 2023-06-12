54b2aee459641a15d63f90fbfcfc567cb5e50ad8
mp-web3 <mp.web3@gmail.com>
2023-06-12 23:48:41 +0200

    test ti check if manager can make a payment request

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -63,4 +63,15 @@ describe('Campaigns', () => {
             assert(err);
         }
     });
+
+    it('allows a manager to make a payment request', async () => {
+        await campaign.methods
+            .createRequest('Buy batteries', 999, accounts[3])
+            .send({
+                from: accounts[0],
+                gas: '1000000'
+            });
+        const request = await campaign.methods.requests(0).call();
+        assert.equal('Buy batteries', request.description);
+    });
 });
