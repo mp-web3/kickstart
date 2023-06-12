79c9c42736379f2bca9edff44c3530d5e4ae455e
mp-web3 <mp.web3@gmail.com>
2023-06-12 09:23:40 +0200

    Before each test we recomplile and deploy Facoty contract

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -7,3 +7,15 @@ const compiledFactory = require('../ethereum/build/Factory.json');
 const compiledCampaign = require('../ethereum/build/Campaign.json');
 
 
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+        .deploy({ data: compiledFactory.bytecode })
+        .send({ from: accounts[0], gas: '1000000'});
+});
