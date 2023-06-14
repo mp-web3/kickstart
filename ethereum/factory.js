df6b180475ced1cad786d271e0748a3f0a9d4b20
mp-web3 <mp.web3@gmail.com>
2023-06-14 17:09:54 +0200

    There was the wrong contract in factory.js

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import Factory from './build/Factory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(Factory.interface),
-    '0x05D2035e858D30b272edAC0a276C757715Fa31A1'
+    '0x0E3Df6C58DCFAd39d4857BdC84952F32e7ec2F05'
 );
 
 export default instance;
