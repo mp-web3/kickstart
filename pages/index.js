50815383c9cb749548bd15457630ae05cdab9dbd
mp-web3 <mp.web3@gmail.com>
2023-06-16 11:33:57 +0200

    added stylesheet as a temporary solution

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Card } from "semantic-ui-react"
 import factory from "../ethereum/factory";
 
 class CampaignIndex extends Component {
@@ -8,8 +9,22 @@ class CampaignIndex extends Component {
     return { campaigns };
   }
 
+  renderCampaigns() {
+    const items = this.props.campaigns.map(address => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true // card stretch for the entire width of the container
+      };
+    });
+
+    return <Card.Group items={items}/>;
+  }
+
   render() {
-    return <div>{this.props.campaigns[0]}</div>;
+    return <div>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css" />
+      {this.renderCampaigns()}</div>;
   }
 }
 
