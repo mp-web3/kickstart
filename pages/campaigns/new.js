b8a8551dbe8d32a45e78b523d929d266b93e953f
mp-web3 <mp.web3@gmail.com>
2023-06-19 11:38:24 +0200

    added onSubmit call createCampaign method from factory

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 import { Form, Button, Input } from 'semantic-ui-react'
 import Layout from "../../components/Layout";
+import factory from "../../ethereum/factory";
+import web3 from "../../ethereum/web3";
 
 class CampaignNew extends Component {
     state = {
         minimumContribution:''
     };
+
+    onSubmit = async (event) => {
+        event.preventDefault();
+        const accounts = await web3.eth.getAccounts();
+
+        await factory.methods
+        .createCampaign(this.state.minimumContribution)
+        .send({
+            from: accounts[0]
+        });
+    };
+
     render() {
         return (
             <Layout>
                 <h1>Create a Campaign</h1>
 
-                <Form>
+                <Form onSubmit={this.onSubmit}>
                     <Form.Field>
                         <label>Minimum Contribution</label>
                         <Input 
@@ -23,6 +37,7 @@ class CampaignNew extends Component {
                     </Form.Field>
 
                     <Button primary>Create!</Button>
+                    
                 </Form>
             </Layout>
         );
