71a41a4b782c16927e7ab862431aeb3f36a4c5e1
mp-web3 <mp.web3@gmail.com>
2023-06-19 09:21:08 +0200

    Created a head tag and moved the semantic-ui stylesheet

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
+import Head from 'next/head';
 import Header from './Header';
 
 const Layout = (props) => {
     return (
         <Container>
+            <Head> {/* Any element placed inside the <Head> Tag is moved to the Head of the Html document */}
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css" />
+            </Head>
+
             <Header />
             {props.children}
         </Container>
