2ea043c28a9b221b22f4c1a49ee4f80c91ff9eab
mp-web3 <mp.web3@gmail.com>
2023-06-19 09:11:24 +0200

    created CampaignNew component

--- pages/index.js
+++ pages/index.js
@@ -28,12 +28,13 @@ class CampaignIndex extends Component {
         <div>
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css" />
           <h3>Open Campaigns</h3>
-          {this.renderCampaigns()}
           <Button
+            floated="right"
             content="Create Campaign"
-           icon="add circle"
-           primary={true}
-          />
+            icon="add circle"
+            primary={true}
+            />
+          {this.renderCampaigns()}
         </div>;
       </Layout>
     )
