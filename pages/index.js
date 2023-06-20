b41007f574f2b8c481179e8498852c71a887d0f9
mp-web3 <mp.web3@gmail.com>
2023-06-20 08:52:09 +0200

    created and fixed show component, fixed route to show component in routes.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import { Card, Button } from "semantic-ui-react"
-import factory from "../ethereum/factory";
-import Layout from "../components/Layout";
-import { Link } from "../routes";
+import React, { Component } from 'react';
+import { Card, Button } from 'semantic-ui-react'
+import factory from '../ethereum/factory';
+import Layout from '../components/Layout';
+import { Link } from '../routes';
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
@@ -32,12 +32,12 @@ class CampaignIndex extends Component {
       <Layout>
         <div>
           <h3>Open Campaigns</h3>
-          <Link route="campaigns/new">
+          <Link route='campaigns/new'>
             <a>
               <Button
-                floated="right"
-                content="Create Campaign"
-                icon="add circle"
+                floated='right'
+                content='Create Campaign'
+                icon='add circle'
                 primary={true}
               />
             </a>
--- routes.js
+++ routes.js
@@ -1,4 +1,7 @@
 const routes = require('next-routes')();
 
+// This is how we tell routes what component to show at a certain url
+routes.add('/campaigns/:address', '/campaigns/show');
+
 module.exports = routes;
 // we need to set up a server.js file and tell it to use server.js
