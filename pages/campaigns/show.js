009426744b570c6dea9e2a5af1bd49d99c711d85
mp-web3 <mp.web3@gmail.com>
2023-06-20 10:52:32 +0200

    added address to show.js

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
+import Layout from '../../components/Layout';
 
 class CampaignShow extends Component {
+    static async getInitialProps(props) {
+        const campaignAddress = props.query.address;
+        return {campaignAddress};
+    }
+
+
     render() {
-        return <h3>Campaign Show</h3>;
+        return (
+            <Layout>
+                <h3>Campaign Details</h3>
+                <p>Address: {this.props.campaignAddress}</p>
+
+            </Layout>
+        );
     }
 }
 
