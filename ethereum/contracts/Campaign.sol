3b4132bb6107225c4b797968fecc2dab2414b743
mp-web3 <mp.web3@gmail.com>
2023-06-20 11:00:54 +0200

    getSummary() returns all the variables we want to show the user when clicks on view campaign

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -14,7 +14,7 @@ contract Factory {
 }
 
 contract Campaign {
-    /*'Request' is not an instance of Request but a definition, like a class that we can use to create instances */
+    //'Request' is not an instance of Request but a definition, like a class that we can use to create instances
     struct Request {
         string description;
         uint value;
@@ -24,20 +24,20 @@ contract Campaign {
         mapping(address => bool) approvals;
     }
 
-    /*'restricted()' when added to a function requires that whoever is calling the funtion has the same address of the manager */
+    //'restricted()' when added to a function requires that whoever is calling the funtion has the same address of the manager
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
 
-    /* 'requests' is an array made of Request type structs */
+    // 'requests' is an array made of Request type structs */
     Request[] public requests;
     address public manager;
     uint public minimumContribution;
     mapping(address => bool) public approvers;
     uint public approversCount;
 
-    /* Constructor function 'Campaign' is executed during contract deployement */
+    // Constructor function 'Campaign' is executed during contract deployement
     function Campaign(uint minimum, address creator) public {
         manager = creator;
         minimumContribution = minimum;
@@ -47,13 +47,13 @@ contract Campaign {
         require(msg.value > minimumContribution);
 
         if (!approvers[msg.sender]) {
-            /* Allow to contribute more than once, but increase the approversCount only the first time */
+            // Allow to contribute more than once, but increase the approversCount only the first time
             approvers[msg.sender] = true;
             approversCount++;
         }
     }
 
-    /* The purpose of 'createRequest' function is to create a new Struct of type Request and add it to requests */
+    // The purpose of 'createRequest' function is to create a new Struct of type Request and add it to requests
     function createRequest(
         string description,
         uint value,
@@ -71,37 +71,40 @@ contract Campaign {
     }
 
     function approveRequest(uint index) public {
-        Request storage request = requests[
-            index
-        ]; /* refactor by declaring a Request object which is equal to the request contained in the requests object at the index passed by the user;*/
+        Request storage request = requests[index]; // refactor by declaring a Request object which is equal to the request contained in the requests object at the index passed by the user;
 
-        require(
-            approvers[msg.sender]
-        ); /* if the sender is not in the approvers mapping this will return false and exit the function */
-        require(
-            !request.approvals[msg.sender]
-        ); /* return false if the user has already voted */
+        require(approvers[msg.sender]); // if the sender is not in the approvers mapping this will return false and exit the function
+        require(!request.approvals[msg.sender]); // return false if the user has already voted
 
         request.approvals[msg.sender] = true;
         request.approvalCount++;
     }
 
     function finalizeRequest(uint index) public restricted {
-        Request storage request = requests[
-            index
-        ]; /* Since we are using several times requests[index], this makes it more coincise */
+        Request storage request = requests[index]; // Since we are using several times requests[index], this makes it more coincise
         /*
         require(!requests[index].complete); 
         requests[index].complete = true;
         */
-        require(
-            request.approvalCount > (approversCount / 2)
-        ); /* to be finilized it needs at leat 50% of contributors approval */
-        require(
-            !request.complete
-        ); /* make sure the request has not been finilized yet */
+        require(request.approvalCount > (approversCount / 2)); // to be finilized it needs at leat 50% of contributors approval
+        require(!request.complete); // make sure the request has not been finilized yet
 
         request.recipient.transfer(request.value);
         request.complete = true;
+    }
+
+    // getSummary() returns all the variables we want to show the user when clicks on view campaign
+    function getSummary()
+        public
+        view
+        returns (uint, uint, uint, uint, address)
+    {
+        return (
+            minimumContribution,
+            this.balance,
+            requests.length,
+            approversCount,
+            manager
+        );
     }
 }
