1ffb941ab4de79167ee91fcb9da97346941b3a3d
mp-web3 <mp.web3@gmail.com>
2023-06-20 08:23:11 +0200

    added routes to campaigns and campaigns/new

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,24 @@
 import Reaxt from 'react';
 import { Menu } from 'semantic-ui-react';
+import { Link } from '../routes';
 
 const Header = (props) => {
     return (
         <Menu style={{ marginTop: '10px' }}>
 
-            <Menu.Item>
-                Crowdcoin
-            </Menu.Item>
+            <Link route="/">
+                {/* The "Link" component wraps its children with a click event handler */}
+                <a className="item">CrowdCoin</a>
+            </Link>
 
             <Menu.Menu position='right'>
-                <Menu.Item>
-                    Campaigns
-                </Menu.Item>
+                <Link route="/">
+                    <a className="item">Campaigns</a>
+                </Link>
 
-                <Menu.Item>
-                    +
-                </Menu.Item>
+                <Link route="/campaigns/new">
+                    <a className="item">+</a>
+                </Link>
 
             </Menu.Menu>
 
