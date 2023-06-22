16d587b03b0e4e585e5b2c9ceb91c5ca977f921b
mp-web3 <mp.web3@gmail.com>
2023-06-22 17:46:02 +0200

    spinning button when attempred transaction and show message error

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -6,12 +6,16 @@ import { Router } from '../routes';
 
 class ContributeForm extends Component {
     state = {
-        value: ''
+        value: '',
+        errorMessage: '',
+        loading: false
     };
 
     onSubmit = async (event) => {
         event.preventDefault();
         const campaign = campaignInstance(this.props.address);
+
+        this.setState({ loading: true, errorMessage:'' });
 
         try {
             const accounts = await web3.eth.getAccounts();
@@ -23,13 +27,14 @@ class ContributeForm extends Component {
             //Refresh the page through a router function
             Router.replaceRoute(`/campaigns/${this.props.address}`);
         } catch (err) {
-
+            this.setState({ errorMessage: err.message });
         }
+        this.setState({ loading: false, value: '' });
     };
 
     render() {
         return (
-            <Form onSubmit={this.onSubmit}>
+            <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                 <Form.Field>
                     <label>Amount to Contribute</label>
                     <Input 
@@ -38,9 +43,14 @@ class ContributeForm extends Component {
                     onChange={event => this.setState({ value: event.target.value })}
                     />
                 </Form.Field>
-                <Button primary>
+
+
+                <Message error header='Oops!' content={this.state.errorMessage} />
+                <Button primary loading={this.state.loading}>
                 Contribute!
                 </Button>
+
+
             </Form>
         );
     };
