b71eef09d0c6cb802f81d0011412e0a0faca0203
mp-web3 <mp.web3@gmail.com>
2023-06-22 17:24:12 +0200

    try to send a contribution onSubmit of ContributeForm

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { Button, Form, Input, Message } from 'semantic-ui-react';
 import campaignInstance from '../ethereum/campaign';
+import web3 from '../ethereum/web3';
 
 class ContributeForm extends Component {
     state = {
         value: ''
     };
 
-    onSubmit = (event) => {
+    onSubmit = async (event) => {
         event.preventDefault();
-        const campaign = campaign(this.props.address);
+        const campaign = campaignInstance(this.props.address);
+
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await campaign.methods.contribute().send({
+                from: accounts[0],
+                value: web3.utils.toWei(this.state.value, 'ether')
+            });
+        } catch (err) {
+
+        }
     };
 
     render() {
