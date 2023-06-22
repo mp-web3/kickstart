1954adcd91fd1e1ec19ff99477eda35c1259c939
mp-web3 <mp.web3@gmail.com>
2023-06-22 17:59:54 +0200

    created a new component RequestIndex to show when a user clicks on 'View Requests'

--- routes.js
+++ routes.js
@@ -1,8 +1,10 @@
 const routes = require('next-routes')();
 
 // This is how we tell routes what component to show at a certain url
-routes.add('/campaigns/new', 'campaigns/new');
-routes.add('/campaigns/:address', '/campaigns/show');
+routes
+    .add('/campaigns/new', 'campaigns/new')
+    .add('/campaigns/:address', '/campaigns/show')
+    .add('/campaigns/:address/requests', '/campaigns/requests/index')
 
 module.exports = routes;
 // we need to set up a server.js file and tell it to use server.js
