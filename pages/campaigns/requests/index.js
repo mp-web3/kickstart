a3387bdf961503aa23f2ae3b922ed1958d590e32
mp-web3 <mp.web3@gmail.com>
2023-06-22 18:23:47 +0200

    styling the Requests component

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
+import { Button, Grid } from 'semantic-ui-react';
+import Layout from '../../../components/Layout';
 
 class RequestIndex extends Component {
     render() {
-        return <h3>Request List</h3>;
+        return (
+            <Layout>
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column width={12}>
+                            <h3>Request List</h3>
+                        </Grid.Column>
+
+                        <Grid.Column width={4}>
+                            <Button primary>
+                                Add Request
+                            </Button>        
+                        </Grid.Column>
+                    </Grid.Row>
+
+                </Grid>
+    
+            </Layout>
+
+        )
     }
 }
 
